7f4d072342fbc7c8ba97f9f3157357220aa2d0a2
xseejx-edu <simone.comin@iiseinaudiscarpa.edu.it>
2026-05-02 12:04:18 +0200

    Pilot

---
+++ shell/src/main/java/io/github/xseejx/colletctorframework/shell/App.java
@@ -0,0 +1,13 @@
+package io.github.xseejx.colletctorframework.shell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
